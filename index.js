78ea0f6543e2591e68afb1a1d2b2493dbe63e5e9
Will Figueiredo <will@wfigueiredo.com>
2018-08-05 19:42:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,3 @@ function theBeatlesPlay(musicians, instruments) {
   return strings;
 }
 
-// function theBeatlesPlay(musicians, instruments) {
-//   var strings = []
-//   for (var i = 0; i < 4; i++) {
-//     strings.push(musicians[i] + " plays " + instruments[i])
-//   }
-//   return strings
-// }
