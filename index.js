e44c0a67533d9d47ab83dbb7a6b61ffc229142bf
Will Figueiredo <will@wfigueiredo.com>
2018-08-05 19:45:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,20 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  
+  var factString = [];
+  var index = 0;
+  while (facts.length > factString.length) {
+    factString.push(facts[index++] + "!!!")
+  }
+  return factString;
 }
+
+
+// function iLoveTheBeatles(number) {
+//   var empty = []
+//   do {
+//     empty.push("I love the Beatles!")
+//     number++
+//   } while (number < 15);
+//   return empty
+// }
