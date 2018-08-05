1a1206a46b3a8474d7250a2ba5a2360693b5503d
Will Figueiredo <will@wfigueiredo.com>
2018-08-05 19:40:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // add solution here
-function theBeatlesPlay(musicians, instrumants) {
-  var strings = [];
+// function theBeatlesPlay(musicians, instrumants) {
+//   var strings = [];
   
-  for (var i = 0; i < 4; i++) {
-    strings.push(musicians[i] + " plays " + instruments[i])
-  }
-  return strings;
-}
-
-// function theBeatlesPlay(musicians, instruments) {
-//   var strings = []
 //   for (var i = 0; i < 4; i++) {
 //     strings.push(musicians[i] + " plays " + instruments[i])
 //   }
-//   return strings
+//   return strings;
 // }
+
+function theBeatlesPlay(musicians, instruments) {
+  var strings = []
+  for (var i = 0; i < 4; i++) {
+    strings.push(musicians[i] + " plays " + instruments[i])
+  }
+  return strings
+}
